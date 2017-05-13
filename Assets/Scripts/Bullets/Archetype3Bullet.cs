8db6122c7b6508b90c0d091a4c6a7472dc9f2df0
unknown <nadjam@320-C4.nad.qc.ca>
2017-05-13 15:34:05 -0400

    archetype3

--- Assets/Scripts/Bullets/Archetype3Bullet.cs
+++ Assets/Scripts/Bullets/Archetype3Bullet.cs
@@ -2,12 +2,34 @@
 
 public class Archetype3Bullet : Bullet
 {
+    public float ExplodeTime = 1.5f;
+    private float m_ExplodeTimer;
+
+    protected override void DoStart()
+    {
+        m_ExplodeTimer = ExplodeTime;
+    }
+
+    protected override void DoUpdate()
+    {
+        m_ExplodeTimer -= Time.deltaTime;
+        if (m_ExplodeTimer < 0)
+        {
+            Explode();
+        }
+    }
+
     protected override void DoOnCollisionEnter(Collision collision)
     {
-        if (Helpers.CheckObjectTag(collision.gameObject, "Player"))
+        if (!Helpers.CheckObjectTag(collision.gameObject, "AI"))
         {
-            Instantiate(ResourceManager.GetPrefab("UnshrinkExplosion"), transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            Explode();
         }
+    }
+
+    private void Explode()
+    {
+        Instantiate(ResourceManager.GetPrefab("UnshrinkExplosion"), transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -5,6 +5,12 @@ public class Bullet : MonoBehaviour
     public float Speed = 5.0f;
     public float DestroyRange = 20.0f;
 
+    void Start()
+    {
+        DoStart();
+    }
+    protected virtual void DoStart() { }
+
     void FixedUpdate()
     {
         transform.position += transform.forward * Speed * Time.deltaTime;
@@ -12,6 +18,12 @@ public class Bullet : MonoBehaviour
         if (transform.position.magnitude > DestroyRange)
             Destroy(gameObject);
     }
+    
+    void Update()
+    {
+        DoUpdate();
+    }
+    protected virtual void DoUpdate() { }
 
     void OnCollisionEnter(Collision collision)
     {
