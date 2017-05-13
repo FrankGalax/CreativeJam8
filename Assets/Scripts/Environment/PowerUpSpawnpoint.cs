937d29d2b7ee00ea3617854a951ad8e2ccb92fe0
unknown <nadjam@320-C4.nad.qc.ca>
2017-05-13 17:02:13 -0400

    calice

---
+++ Assets/Scripts/Environment/PowerUpSpawnpoint.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PowerUpSpawnpoint : MonoBehaviour
+{
+    public float Powerup1SpawnRate = 0.5f;
+    public float Powerup2SpawnRate = 0.5f;
+    public float SpawnCooldown = 10.0f;
+
+    private float m_SpawnTimer;
+
+    void Awake()
+    {
+        DestroyImmediate(transform.Find("HelperGraphic").gameObject);
+    }
+
+    void Start()
+    {
+        m_SpawnTimer = SpawnCooldown;
+    }
+
+    void Update()
+    {
+        m_SpawnTimer -= Time.deltaTime;
+        if (m_SpawnTimer > 0)
+            return;
+
+        float random = UnityEngine.Random.value;
+        GameObject archetypePrefab = null;
+        if (random < Powerup1SpawnRate)
+        {
+            archetypePrefab = ResourceManager.GetPrefab("Powerup1");
+        }
+        else
+        {
+            archetypePrefab = ResourceManager.GetPrefab("Powerup2");
+        }
+
+        Instantiate(archetypePrefab, transform.position, Quaternion.identity).GetComponent<Powerup>().Spawnpoint = this;
+
+        m_SpawnTimer = SpawnCooldown;
+        enabled = false;
+    }
+}
