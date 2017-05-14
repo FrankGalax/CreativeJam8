725b9ecb3043af962127a5e5846ba831ab9c159a
unknown <nadjam@320-C4.nad.qc.ca>
2017-05-14 13:18:24 -0400

    interaction ui

--- Assets/Scripts/Environment/UnlockInteraction.cs
+++ Assets/Scripts/Environment/UnlockInteraction.cs
@@ -6,6 +6,7 @@ public class UnlockInteraction : MonoBehaviour
     public Barrier Barrier;
     public int Cost;
     public Transform MovePlayer;
+    public Texture Texture;
 
     private bool m_PlayerInside;
     private PlayerController m_PlayerController;
@@ -18,6 +19,19 @@ public class UnlockInteraction : MonoBehaviour
     void Start()
     {
         m_PlayerInside = false;
+    }
+
+    void OnGUI()
+    {
+        if (Texture != null && WaveManager.Instance.IsCooldown && Barrier != null && !Barrier.Unlocked)
+        {
+            int width = 100;
+            int height = 100;
+            Rect rect = new Rect(Camera.main.WorldToScreenPoint(transform.position), new Vector2(100, 100));
+            rect.y = Screen.height - rect.y - height / 2;
+            rect.x -= width/2;
+            GUI.DrawTexture(rect, Texture);
+        }
     }
 
     void OnTriggerEnter(Collider collider)
