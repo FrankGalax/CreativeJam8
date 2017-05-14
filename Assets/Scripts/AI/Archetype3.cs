25d691f43be1bfe62ea9c932eaa62401679fae7d
unknown <nadjam@320-C4.nad.qc.ca>
2017-05-14 15:31:10 -0400

    sasda

--- Assets/Scripts/AI/Archetype3.cs
+++ Assets/Scripts/AI/Archetype3.cs
@@ -9,18 +9,26 @@ public class Archetype3 : AI
     public Vector3 GunOffset;
    
     private float m_FireTimer;
+    private Animator m_Animator;
+
+    protected override void DoStart()
+    {
+        m_Animator = GetComponentInChildren<Animator>();
+    }
 
     protected override void DoUpdate()
     {
         if (m_FireTimer > 0)
         {
             m_FireTimer -= Time.deltaTime;
+            m_Animator.SetBool("Shoot", false);
         }
         else
         {
             Vector3 position = transform.position + transform.TransformVector(GunOffset);
             Instantiate(ResourceManager.GetPrefab("Archetype3Bullet"), position, transform.rotation);
             m_FireTimer = FireCooldown;
+            m_Animator.SetBool("Shoot", true);
         }
 
         List<AI> ais = FindObjectsOfType<AI>().Where(p => p.IsShrunk).ToList();
